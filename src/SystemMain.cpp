ed1ea1ab4dc15852ae78a9c9a9d52baee5b33c8c
JamesSG2 <Jamor.0022@gmail.com>
2024-09-26 15:57:38 -0400

    Set up the project repository and configured the basic back-end framework.

--- src/SystemMain.cpp
+++ src/SystemMain.cpp
@@ -1,11 +1,20 @@
 //
-// Created by jamor on 9/26/2024.
+// Created by james on 9/26/2024.
 //
 
 #include <iostream>
+#include <vector>
+#include "../inlcude/Tab.h"
+#include "../inlcude/Account.h"
 
 int main() {
-    // Your code goes here
-    std::cout << "Hello, World!" << std::endl;
+
+    //Define Fields:
+    std::vector<Tab> tabs{};
+    std::vector<Account> accounts{};
+    std::vector<int> visualizationSettings{}; //Will probably want this to have its own class later
+    std::vector<std::vector<std::vector<int>>> rawData{}; //3D vector (x,y data for z different sensors/motors)
+
+    std::cout << "Field Declarations were Successful" << std::endl;
     return 0;
 }
