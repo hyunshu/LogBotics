b87739b2fd183666d8543df0ec36968dafb4707c
Roger Braun <roger.t.braun@gmail.com>
2024-09-26 14:05:42 -0400

    first commit

---
+++ FRC-App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FRC_App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ FRC-App/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace FRC_App;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnSubmitClicked(object sender, EventArgs e)
+	{
+		LoginBtn.Text = "Logging in...";
+		SemanticScreenReader.Announce(LoginBtn.Text);
+
+		string username = UserNameEntry.Text;
+		string password = UserPasswordEntry.Text;
+
+		if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password)) {
+			Navigation.PushAsync(new MainPage());
+		} else {
+			LoginBtn.Text = "Missing info!";
+			SemanticScreenReader.Announce(LoginBtn.Text);
+		}
+	}
+}
+
