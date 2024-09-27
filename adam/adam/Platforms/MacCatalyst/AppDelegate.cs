655de5313e2c463e9761400e4a1a73cbb3e203d3
HyunShu <hyunshu@hotmail.com>
2024-09-26 23:50:04 -0400

    adam
    default maui app that runs

---
+++ adam/adam/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace adam;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
