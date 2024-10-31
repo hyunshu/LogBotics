c73287c3f767c929787257f43e13722cb3755602
JennaRigdon <jennacrigdon@gmail.com>
2024-10-31 17:46:33 -0400

    added skeleton map page

---
+++ FRC-App/Front-Back-Interface/MapPage.xaml.cs
@@ -0,0 +1,15 @@
+using Microsoft.Maui.Controls;
+using System.ComponentModel;
+using FRC_App.Models;
+using FRC_App.Services;
+
+namespace FRC_App
+{
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
