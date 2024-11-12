62e00ea180ccaec4a4e3c804ebce17a21abe2acf
Roger Braun <roger.t.braun@gmail.com>
2024-11-12 00:29:50 -0500

    finally added all functionality we had for microcharts... now for livecharts2

--- FRC-App/Front-Back-Interface/AddPlotPage.xaml.cs
+++ FRC-App/Front-Back-Interface/AddPlotPage.xaml.cs
@@ -1,11 +1,11 @@
 using FRC_App.Models;
 using System.Collections.ObjectModel;
-using Microcharts.Maui;
-using Microcharts;
-using LiveChartsCore;
-using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
 using LiveChartsCore.SkiaSharpView.Maui;
+using SkiaSharp;
+using LiveChartsCore.SkiaSharpView.SKCharts;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Drawing;
+using LiveChartsCore.SkiaSharpView.VisualElements;
 
 namespace FRC_App;
 
@@ -16,6 +16,8 @@ public partial class AddPlotPage : ContentPage
 	public DataContainer userData { get; private set; }
 	public Dictionary<string, Plot> plotDict { get; set; }
 	public ObservableCollection<CartesianChart> chartCollection { get; set; }
+	public ObservableCollection<string> chartTitles { get; set; } 
+	public ObservableCollection<Grid> chartGrids { get; set; } 
 	
 	public int numPlots;
 	public AddPlotPage(User user)
@@ -23,9 +25,28 @@ public partial class AddPlotPage : ContentPage
 		InitializeComponent();
 		currentUser = user;
 		plotDict = new Dictionary<string, Plot>{};
-		chartCollection = new ObservableCollection<CartesianChart>();
+		chartCollection = new ObservableCollection<CartesianChart>
+		{
+			chart1,
+			chart2,
+			chart3,
+			chart4,
+			chart5,
+			chart6
+		};
+
+		chartGrids = new ObservableCollection<Grid> 
+		{
+			ChartGrid1,
+			ChartGrid2,
+			ChartGrid3,
+			ChartGrid4,
+			ChartGrid5,
+			ChartGrid6
+		};
 		numPlots = 0;
 		BindingContext = this;
+		chartTitles = new ObservableCollection<string>();
 	}
 
 	private async void AddPlot(object sender, EventArgs e) {
@@ -88,55 +109,157 @@ public partial class AddPlotPage : ContentPage
 			await DisplayAlert("Warning", "You have selected the x-axis to be the same as the y-axis. This is an unusual request.", "OK");
 		}
 
-		try {
-			plotDict.Add(newPlot.Title, newPlot);
-		} catch (ArgumentException) {
-			newPlot.Title = newPlot.Title + "(" + numPlots.ToString() + ")";
-			plotDict.Add(newPlot.Title, newPlot);
-		}
+		// try {
+		// 	plotDict.Add(newPlot.Title, newPlot);
+		// } catch (ArgumentException) {
+		// 	newPlot.Title = newPlot.Title + "(" + numPlots.ToString() + ")";
+		// 	plotDict.Add(newPlot.Title, newPlot);
+		// }
 		renderNewPlot(newPlot);
 
 		TypesStack.IsVisible = false;
 	}
 
 	private void renderNewPlot (Plot newPlot) {
-		CartesianChart newChart = newPlot.GetLineChart();
+		CartesianChart newChart = newPlot.GetScatterChart();
 
-			lvc1.Series = newChart.Series;
+		for (int i = 0; i < chartCollection.Count; i++) {
+			var currChart = chartCollection[i];
+			var currGrid = chartGrids[i];
+			if (currGrid.IsVisible == false) {
+				currChart.Series = newChart.Series;
+				currChart.Title = newChart.Title; 
+				currGrid.IsVisible = true;
+				break;
+			}
+		}
 			
-			lvc1.IsVisible = true;
-			chartCollection.Add(newChart);
-			numPlots++;
-		}
-
-	private async void DeletePlot(object sender, EventArgs e) {
-		if (numPlots <= 0) {
-			await DisplayAlert("Error", "No plots to delete.", "OK");
-			return;
-		}
-
-
-
+		numPlots++;
 	}
 
-	private async void DeleteSelectedPlot(object sender, EventArgs e) {
-		string selectedPlot = DeleteDropDown.SelectedItem?.ToString();
 
-		if (string.IsNullOrEmpty(selectedPlot)) {
-			await DisplayAlert("Error", "Must select a plot to delete", "OK");
-			return;
-		}
-
+	private async void OnDeleteChart1Clicked(object sender, EventArgs e) {
+		chartGrids[0].IsVisible = false;
 		numPlots--;
-	
+	}
 
+	private async void OnDeleteChart2Clicked(object sender, EventArgs e) {
+		chartGrids[1].IsVisible = false;
+		numPlots--;
+	}
+
+	private async void OnDeleteChart3Clicked(object sender, EventArgs e) {
+		chartGrids[2].IsVisible = false;
+		numPlots--;
+	}
+
+	private async void OnDeleteChart4Clicked(object sender, EventArgs e) {
+		chartGrids[3].IsVisible = false;
+		numPlots--;
+	}
+
+	private async void OnDeleteChart5Clicked(object sender, EventArgs e) {
+		chartGrids[4].IsVisible = false;
+		numPlots--;
+	}
+
+	private async void OnDeleteChart6Clicked(object sender, EventArgs e) {
+		chartGrids[5].IsVisible = false;
+		numPlots--;
 	}
 
 	private async void ExportToJpeg(object sender, EventArgs e) {
+		if (numPlots == 0) {
+			await DisplayAlert("Error", "There are no plots to export.", "OK");
+			return;
+		}
 
+		int exportCount = 0;
+		for (int i = 0; i < chartCollection.Count; i++) {
+			CartesianChart chart = chartCollection[i];
+			if (chartGrids[i].IsVisible) {
+				string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+				var skChart = new SKCartesianChart
+				{
+					Series = chart.Series,   
+					Title = chart.Title,      
+				};
+
+				var filePath = Path.Combine(desktopPath, "chart" + exportCount.ToString() + ".jpeg");
+				skChart.SaveImage(filePath);
+				exportCount++;
+			}
+		}
 	}
 
 	private async void ExportToPdf(object sender, EventArgs e) {
+    if (numPlots == 0) {
+        await DisplayAlert("Error", "There are no plots to export.", "OK");
+        return;
+    }
 
-	}
+    string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+    using (var pdf = new PdfDocument()) {
+        for (int i = 0; i < chartCollection.Count; i++) {
+            CartesianChart chart = chartCollection[i];
+
+            if (chartGrids[i].IsVisible) {
+                var skChart = new SKCartesianChart
+                {
+                    Series = chart.Series,
+                    Title = chart.Title,
+                };
+
+                // Increase bitmap size for higher resolution
+                int width = 1200;  // Increase width
+                int height = 800;  // Increase height
+
+                using (var bitmap = new SKBitmap(width, height))
+                using (var canvas = new SKCanvas(bitmap)) {
+                    canvas.Clear(SKColors.White);
+
+                    // Draw content and fit it entirely
+                    skChart.DrawOnCanvas(canvas);
+
+                    using (var image = SKImage.FromBitmap(bitmap))
+                    using (var data = image.Encode(SKEncodedImageFormat.Png, 100)) {
+                        // Add a new page to the PDF
+                        var pdfPage = pdf.AddPage();
+                        var graphics = XGraphics.FromPdfPage(pdfPage);
+
+                        using (var ms = new MemoryStream(data.ToArray()))
+                        using (var img = XImage.FromStream(() => new MemoryStream(ms.ToArray()))) {
+                            // Calculate scaling to maintain aspect ratio
+                            double pdfAspect = pdfPage.Width / pdfPage.Height;
+                            double imageAspect = img.PixelWidth / (double)img.PixelHeight;
+
+                            double newWidth, newHeight;
+                            if (pdfAspect > imageAspect) {
+                                // Fit by height
+                                newHeight = pdfPage.Height;
+                                newWidth = img.PixelWidth * (newHeight / img.PixelHeight);
+                            } else {
+                                // Fit by width
+                                newWidth = pdfPage.Width;
+                                newHeight = img.PixelHeight * (newWidth / img.PixelWidth);
+                            }
+
+                            double xPosition = (pdfPage.Width - newWidth) / 2;
+                            double yPosition = (pdfPage.Height - newHeight) / 2;
+
+                            // Draw the image on the PDF page
+                            graphics.DrawImage(img, xPosition, yPosition, newWidth, newHeight);
+                        }
+                    }
+                }
+            }
+        }
+
+        var pdfPath = Path.Combine(desktopPath, "charts.pdf");
+        pdf.Save(pdfPath);
+        await DisplayAlert("Export Successful", $"PDF saved to {pdfPath}", "OK");
+    }
+}
+
 }
